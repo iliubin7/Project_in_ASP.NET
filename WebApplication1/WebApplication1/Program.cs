98312340ef82d69ec4d56bb3d9e007d29cff9723
iliubin7 <257798@student.pwr.edu.pl>
2023-05-17 00:11:29 +0300

    Add database and game class

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<WebApplication1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApplication1Context") ?? throw new InvalidOperationException("Connection string 'WebApplication1Context' not found.")));
 
 var app = builder.Build();
 
