20eaf4b778e6503d30bc2e596e4a75c996ed1b1a
iliubin7 <257798@student.pwr.edu.pl>
2023-05-17 13:39:51 +0300

    Starting work on Add actual currency rates

--- WebApplication1/WebApplication1/Models/Alcohol.cs
+++ WebApplication1/WebApplication1/Models/Alcohol.cs
@@ -13,4 +13,22 @@ namespace WebApplication1.Models
         public string Country { get; set; }
         public decimal Price { get; set; }
     }
+    internal class Currency
+    {
+        public int ID { set; get; }
+        public string table { get; set; }       // typ tabeli 
+        public string currency { get; set; }    // nazwa waluty 
+        public string code { get; set; }        // kod walutowy
+        public List<Rate> rates { get; set; }   // lista kursów poszczególnych walut w tabeli 
+
+    }
+
+    public class Rate
+    {
+        public int rateID { get; set; }
+        public float mid { get; set; } // kurs średni {tabela A i tabela B}
+        public float bid { get; set; } // kurs kupna {tabela C}
+        public float ask { get; set; } // kurs sprzedaży {tabela C}
+        public string effectiveDate { get; set; }  // data publikacji 
+    }
 }
--- WebApplication1/WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using WebApplication1.Models;
 
 namespace WebApplication1.Pages
 {
@@ -16,5 +19,24 @@ namespace WebApplication1.Pages
         {
 
         }
-    }
+
+        private async Task<string> downloadData()
+        {
+            string code = "USD";
+            string table = "A";
+            HttpClient client = new HttpClient();
+            string call = "http://api.nbp.pl/api/exchangerates/rates/" + table + "/" + code + "/?format=json";
+            string json = await client.GetStringAsync(call);
+            return json;
+        }
+        private async Task<string> ShowCurRate()
+        {
+            string json = await downloadData();
+            Currency cur = JsonConvert.DeserializeObject<Currency>(json);
+            // label4.Text = cur.currency + "\nKurs kupna: " + cur.rates[0].bid.ToString() + " PLN  " + "\nKurs sprzedaży: " + cur.rates[0].ask.ToString() + " PLN";
+            string m = cur.rates[0].bid.ToString();
+            return m;
+        }
+
+        }
 }
